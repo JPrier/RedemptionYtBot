2303b313d70a0bbdb225939c7b60a60771a4ea4d
Joshua Prier <JoshKPrier@gmail.com>
2024-11-20 14:13:22 -0800

    Add new service code and gitignore

--- service/src/handler/checkVideos.py
+++ service/src/handler/checkVideos.py
@@ -1,2 +1,15 @@
+from googleapiclient.discovery import build
+
+API_KEY = os.getenv('YOUTUBE_API_KEY')
+CHANNEL_IDS = os.getenv('YOUTUBE_CHANNEL_IDS')
+
 def process(event, context):
-  print(event)
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+    for channel in CHANNEL_IDS:
+        request = youtube.search().list(
+            part='snippet',
+            channelId=CHANNEL_ID,
+            maxResults=5,  # Fetch up to 5 latest videos
+            order='date'
+        )
+        print(request.execute())
